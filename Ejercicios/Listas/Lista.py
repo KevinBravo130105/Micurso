97cc86631dfc545eda829bfadd5bebfb5dd2087d
Kevin Sinai Bravo Canchola <kevinsinaibravocanchola708@gmail.com>
2021-09-30 20:31:06 -0500

    Sesion 5

---
+++ Ejercicios/Listas/Lista.py
@@ -0,0 +1,39 @@
+# import sys 
+
+# lista = []
+# mi_tuple = ()
+
+# print(type(lista))
+# print(type(mi_tuple))
+
+
+# """Saber el tamaño de una lista o un tuple"""
+# print(f"Tamaño en memoria de la lista: {sys.getsizeof(lista)}")
+# print(f"Tamaño en memoria de la lista: {sys.getsizeof(mi_tuple)}")
+
+
+# """Acceder a los valores de una lista o un tuple por medio de su indice"""
+# lista2 = [1, 2, 3, 4, 5]
+# tuple = (6, 7, 7, 5, 3)  #Indice en python inicia de 0 1 2 3....
+
+# print(lista2[4])
+# print(tuple[3])
+
+# #Acceder al ultimo valor de una lista o tuple
+# print(lista2[-1])
+# print(tuple[-1])
+
+#len() Es lo extenso de un objeto
+#print(len(lista2))
+#print(len(tuple))
+
+#Editar valores de una lista
+lista = [9, 8, 7, 6, 5]
+tuple = (7, 7, 6, 1)
+
+lista.insert(0, 10)
+lista.append("Hola")
+
+print(lista.reverse)
+#print(tuple [-1])
+
