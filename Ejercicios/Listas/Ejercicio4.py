af9629f55f7884455c5af94e45620d9cfaf00aae
Kevin Sinai Bravo Canchola <kevinsinaibravocanchola708@gmail.com>
2021-10-13 12:41:17 -0500

    Ejercicios semana 4

--- Ejercicios/Listas/Ejercicio4.py
+++ Ejercicios/Listas/Ejercicio4.py
@@ -1,4 +1,4 @@
-nombres = ["Karen", "Leslie", "Raymundo"]
+nombres = ["Karen", "Leslie", "Jessica"]
 
 for nombre in nombres:
     print(f"Hola {nombre} ¿Cómo te encuentas el dia de hoy? Quería invitarte a mi fiesta de cumpleaños, este jueves a las 3 de la tarde")
@@ -9,9 +9,18 @@ nombres.pop(2)
 nombres.insert(2, "Ximena")
 print(f"Hola {nombres [2]} ¿Cómo te encuentas el dia de hoy? Quería invitarte a mi fiesta de cumpleaños, este jueves a las 3 de la tarde\n")
 
-nombres.insert(0, "Jessica")
+nombres.insert(0, "Raymundo")
 nombres.insert(2, "Luis")
 nombres.insert(5, "Mayavi")
 
 for nombre in nombres:
     print(f"Hola {nombre} ¿Cómo te encuentas el dia de hoy? Quería invitarte a mi fiesta de cumpleaños, este jueves a las 3 de la tarde")
+
+print("\n")
+while len(nombres) > 2:
+    nombre = nombres.pop()
+    print(f"Lo siento {nombre}, no estas invitado por falta de asientos")
+
+print("\n")
+for nombre in nombres:
+    print(f"Hola {nombre}\nGracias por asistir a mi fiesta, ¿cómo te parecio la cena?")
