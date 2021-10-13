d501397c0c9af76bf2fb04d6454f929596b92858
Kevin Sinai Bravo Canchola <kevinsinaibravocanchola708@gmail.com>
2021-10-13 18:04:25 -0500

    Semana 6

---
+++ Ejercicios/Listas/Semana 6/Ejercicio4.py
@@ -0,0 +1,8 @@
+lista = ["Hola", "Mundo", "Mágico", "Quiero", "Un", "Helado", "Esta", "Hoy"]
+primeros_tres = lista[:3]
+proximos_tres = lista[3:6]
+ultimos_tres = lista[5:]
+
+print(primeros_tres)
+print(proximos_tres)
+print(ultimos_tres)
